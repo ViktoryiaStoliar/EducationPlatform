2ee4df966df1178758d037b1d1036a43cd0db10c
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-08-08 23:22:55 +0300

    React

--- client/src/components/PreviewInfo/PreviewInfo.js
+++ client/src/components/PreviewInfo/PreviewInfo.js
@@ -11,22 +11,46 @@ function PreviewInfo() {
                     <div className={style.btnAbout}>About platform</div>
                     <div className={style.quantity}>
                         <div className={style.top}>
-                        <div className={style.arrow}></div>
-                        <p className={style.number}>
-                            600<span>+</span>
-                        </p>
+                            <div className={style.arrow}></div>
+                            <p>
+                                600 <span>+</span>
+                            </p>
                         </div>
-                    <p className={style.underNumber}>Students</p>
+                        <p className={style.underNumber}>Students</p>
                     </div>
                 </div>
                 <div className={style.img}></div>
             </div>
 
-            <div className=''></div>
+
+            <div className={style.wrapper}>
+
+                <div className={style.learnLanguage}>
+
+                    <div className={style.sittingGirl}></div>
+
+                    <div class={style.secondPath}>
+
+                        <h1 className={style.bigL}>Learn a language in a playful way</h1>
+                        <p className={style.smallL}>Make learning programming languages more fun with mini-games</p>
+
+                        <div className={style.img}>
+                            <div className={style.sprint}>
+                            </div>
+                            <div className={style.tasks}>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
             <div className=''></div>
             <div className=''></div>
         </>
     )
 }
 
-export default PreviewInfo; 
+export default PreviewInfo;
+
